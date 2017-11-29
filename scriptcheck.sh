c07bdce17523898153a9701685a07b58c9c3e9bc
David Tinoco <daviid.tc@gmail.com>
2017-11-29 16:41:44 +0100

    Update scriptcheck.sh

--- scriptcheck.sh
+++ scriptcheck.sh
@@ -1,30 +1,61 @@
 #! /bin/bash
 
+bash /home/david/Documentos/ASIR/Segundo/HLC/libvirt/inicio.sh
+
 stat1=`virsh list --all | grep mv1 | tr -s " " | cut -d " " -f 4`
 stat2=`virsh list --all | grep mv2 | tr -s " " | cut -d " " -f 4`
 
-if [[ $stat1 == 'running' ]]
-then
-	ip1=`virsh net-dhcp-leases br1 | grep mv1 | tr -s " " | cut -d " " -f 6 | cut -d "/" -f 1`
+statmem1='ok'
+statmem2='ok'
+
+ip1=`virsh net-dhcp-leases br1 | grep mv1 | tr -s " " | cut -d " " -f 6 | cut -d "/" -f 1`
+
+while [[ $statmem1 == 'ok' ]]; do
 
 	memava=`ssh -i /home/david/.ssh/jupiter root@$ip1 cat /proc/meminfo | grep MemAvailable | tr -s " " | cut -d " " -f 2`
 
 	if [[ $memava -lt "30000" ]]
 	then
-		echo "Cambiando de máquina"
-		bash /home/david/Documentos/ASIR/Segundo/HLC/libvirt/script.sh
+
+		sleep 10
+
+		memava=`ssh -i /home/david/.ssh/jupiter root@$ip1 cat /proc/meminfo | grep MemAvailable | tr -s " " | cut -d " " -f 2`
+
+		if [[ $memava -lt "30000" ]]
+		then
+
+			echo "Cambiando de máquina"
+
+			bash /home/david/Documentos/ASIR/Segundo/HLC/libvirt/script.sh
+
+			statmem1='bad'
+		fi
 	fi
 
-elif [[ $stat2 == 'running' ]]
-then
-	ip2=`virsh net-dhcp-leases br1 | grep mv2 | tr -s " " | cut -d " " -f 6 | cut -d "/" -f 1`
+done
 
-	memava2=`ssh -i /home/david/.ssh/jupiter root@$ip2 cat /proc/meminfo | grep MemAvailable | tr -s " " | cut -d " " -f 2`
+ip2=`virsh net-dhcp-leases br1 | grep mv2 | tr -s " " | cut -d " " -f 6 | cut -d "/" -f 1`
+
+while [[ $statmem2 == 'ok' ]]; do
 	
-	if [[ $memava2 -lt "60000" ]]
+	memava=`ssh -i /home/david/.ssh/jupiter root@$ip2 cat /proc/meminfo | grep MemAvailable | tr -s " " | cut -d " " -f 2`
+	
+	if [[ $memava -lt "60000" ]]
 	then
-		echo "Aumentando la memoria"
-		virsh setmem mv2 2G --live
-	fi
 
-fi
+		sleep 10
+
+		memava=`ssh -i /home/david/.ssh/jupiter root@$ip2 cat /proc/meminfo | grep MemAvailable | tr -s " " | cut -d " " -f 2`
+
+		if [[ $memava -lt "60000" ]]
+		then
+
+			echo "Aumentando la memoria"
+
+			virsh setmem mv2 2G --live
+
+			statmem2='aumented'
+		fi
+
+	fi
+done
